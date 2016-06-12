c697c28ef7518b3459a2ff75d7d70583d7459ec7
user <user@localhost>
2016-06-12 21:04:28 +0300

    Добавил функцию инклюд

--- src/main/resources/js/util/prefix.js
+++ src/main/resources/js/util/prefix.js
@@ -1,2 +1,2 @@
-var data = JSON.parse(dataStr); var console = {log: function(str){print(str);}};
+var data = JSON.parse(dataStr); var console = {log: function(str){print(str);}}; function include(name){load("lib/"+name);}
 var ret = {i: JSON.stringify(data)};
