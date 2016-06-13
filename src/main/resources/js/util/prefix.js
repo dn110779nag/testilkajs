4b38d5b82cc2c1335200f1f95986d19dcb168d88
nova <nova@novax.apb.it.loc>
2016-06-13 09:43:39 +0300

    подправил реквайр под формат конвейера

--- src/main/resources/js/util/prefix.js
+++ src/main/resources/js/util/prefix.js
@@ -1,3 +1,3 @@
-var data = JSON.parse(dataStr); var console = {log: function(str){print(str);}}; function include(name){load("lib/"+name);}
+var data = JSON.parse(dataStr); var console = {log: function(str){print(str);}}; function require(path){load(path);}
 var data = JSON.parse(dataStr); var console = {log: function(str){print(str);}}; delete data.stack;
 var ret = {i: JSON.stringify(data)};
